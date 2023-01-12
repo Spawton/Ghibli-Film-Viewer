783214e81fa26cf2f92c09bd546fbe1f8c58f945
Amy Spawton <amyspawton@hotmail.co.uk>
2023-01-12 16:20:31 +0000

    A lovely Studio Ghibli film viewer

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Ghibli-Film-Viewer
-A lovely web page to view Ghibli movies
+
+NOTE: The public Studio Ghibli API that I'm using is current undergoing maintenance/ having some issues, hence why the results aren't being generated. I appreciate your patience as this issue gets fixed, as it is out my hands.
+
+---
+
+A lovely responsive web app to view Studio Ghibli movies using the Ghibli REST API. I'm sure you have seen Howl's moving castle, but there's so many more gems to choose from!
+
+[Try it out here](https://starlit-babka-7ac42c.netlify.app/)!
+
+![Discover Ghibli Screenshot](../Ghibli-Film-Viewer/assets/Screenshot.png)
+
+
+
+---
+
+## How It's Made
+**Tech used:** HTML, CSS, JavaScript, Ghibli API
+
+This app uses the Ghibli REST API, arranging, and displaying the data in a simple, and elegant interface.
+
+Just try to tell me those buttons aren't satisfying. I dare you.
+
+
+
+---
---
+++ js/main.js
@@ -0,0 +1,81 @@
+// ***Grab HTML Elements***
+// Buttons:
+leftBtn = document.querySelector('#leftBtn')
+rightBtn = document.querySelector('#rightBtn')
+
+// Output Fields:
+titleJP = document.querySelector('#titleJP')
+titleRomanized = document.querySelector('#titleRomanized')
+title = document.querySelector('#title')
+poster = document.querySelector('#poster')
+
+year = document.querySelector('#year')
+director = document.querySelector('#director')
+producer = document.querySelector('#producer')
+rtScore = document.querySelector('#rtScore')
+runtime = document.querySelector('#runtime')
+
+description = document.querySelector('#description')
+
+
+// ***Event Listeners***
+leftBtn.addEventListener('click', function() {browseLeft()})
+rightBtn.addEventListener('click', function() {browseRight()})
+
+
+// ***Initialization***
+// Declare global data storage object
+let movies
+// Declare index binding
+let browseIndex
+const music = new Audio('26 -ending- the promise of the world _ merry-go-round of life.mp3')
+
+// Grab data from API
+fetch(`https://ghibliapi.herokuapp.com/films`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        movies = data
+
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+
+function browseLeft() {
+    music.play()
+    document.querySelector('.titleBox').classList.remove('hidden')
+    document.querySelector('.detailsTable').classList.remove('hidden')
+    //If first click, or at end of array, set movie to be displayed to last in movies array
+    if(browseIndex === undefined || browseIndex <= 0) browseIndex = movies.length - 1 
+    else browseIndex -= 1
+    updateDisplay()   
+}
+
+function browseRight() {
+    music.play()
+    document.querySelector('.titleBox').classList.remove('hidden')
+    document.querySelector('.detailsTable').classList.remove('hidden')
+    //If first click, or at end of array, set movie to be displayed to 0
+    if(browseIndex === undefined || browseIndex >= movies.length - 1) browseIndex = 0 
+    else browseIndex += 1
+    updateDisplay()
+}
+
+function updateDisplay() {
+    titleJP.innerText = movies[browseIndex].original_title
+    titleRomanized.innerText = movies[browseIndex].original_title_romanised
+    title.innerText = movies[browseIndex].title
+    poster.src = movies[browseIndex].image
+    
+    year.innerText = movies[browseIndex].release_date
+    director.innerText = movies[browseIndex].director
+
+    if(browseIndex === 20) producer.innerText = 'Toshio Suzuki'
+    else producer.innerText = movies[browseIndex].producer
+
+    rtScore.innerText = movies[browseIndex].rt_score
+    runtime.innerText = `${movies[browseIndex].running_time} minutes`
+
+    description.innerText = movies[browseIndex].description
+}
